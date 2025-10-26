ade9aa46e4a52899348ce7f70fa927b37930e35d
NishadPatil008 <nishadpatil2008@gmail.com>
2025-10-26 17:45:43 +0530

    int main putted at top and solved errors

--- binary_tree.c
+++ binary_tree.c
@@ -1,89 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Node {
+struct node {
     int data;
-    struct Node *left;
-    struct Node *right;
-} Node;
-// 2. Function to create a new Node
-Node* createNode(int value) {
-    // Allocate memory for the new node
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed!\n");
-        exit(1);
-    }
-    
-    // Initialize the node with the given value
-    newNode->data = value;
-    newNode->left = NULL; // As per the 'Linked Representation' concept in your notes
-    newNode->right = NULL;
-    return newNode;
-}
-
-// 3. Function to insert a new value into the BST
-// This follows the BST rule: (L < Root < R)
-Node* insert(Node* root, int value) {
-    // If the tree is empty, return a new node as the root
-    if (root == NULL) {
-        return createNode(value);
-    }
-
-    // Traverse the tree to find the correct insertion spot
-    if (value < root->data) {
-        // Go to the left subtree if the value is smaller
-        root->left = insert(root->left, value);
-    } else if (value > root->data) {
-        // Go to the right subtree if the value is larger
-        root->right = insert(root->right, value);
-    }
-    // If the value is equal, we typically do nothing (no duplicates allowed in a simple BST)
-
-    // Return the (unchanged) root pointer
-    return root;
-}
-
-// 4. Function to perform Inorder Traversal
-// This traversal visits nodes in the order: Left -> Root -> Right
-// It is used to print the BST elements in SORTED order.
-void inorderTraversal(Node* root) {
-    if (root != NULL) {
-        inorderTraversal(root->left);    // 1. Recurse on the left subtree
-        printf("%d ", root->data);      // 2. Visit the current node (print data)
-        inorderTraversal(root->right);   // 3. Recurse on the right subtree
-    }
-}
-
-
-// Main function to test the BST
+    struct node *left, *right;
+};
+struct node *root = NULL;
 int main() {
-    Node* root = NULL;
-    
-    // Sample data to insert (similar to the data in your example: 85, 10, 78, 12, 54, 11, 9, 30)
-    int elements[] = {50, 30, 70, 20, 40, 60, 80};
-    int n = sizeof(elements) / sizeof(elements[0]);
-
-    printf("Inserting elements into the BST: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", elements[i]);
-        root = insert(root, elements[i]);
+    int choice;
+    while (1) {
+        printf("\n1.Insert  2.Inorder  3.Preorder  4.Postorder  5.Search  6.Exit\nChoice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: 
+            insert(); break;
+            case 2: 
+            in_order(root); 
+            printf("\n"); break;
+            case 3: 
+            pre_order(root); 
+            printf("\n"); break;
+            case 4: 
+            post_order(root); 
+            printf("\n"); break;
+            case 5: 
+            search_element(); break;
+            case 6: 
+            return 0;
+            default: 
+            printf("Invalid choice\n");
+        }
     }
-    printf("\n");
-
-    printf("---BST Constructed---\n");
-    // The structure will look like:
-    //         50
-    //       /    \
-    //     30      70
-    //    /  \    /  \
-    //  20   40 60    80
-
-    // Perform Inorder Traversal
-    printf("Inorder Traversal (Sorted Order): ");
-    inorderTraversal(root);
-    printf("\n");
-    
-    // Note: In a production environment, you would also need a function to free the allocated memory.
-
     return 0;
+}
+struct node* create(int data) {
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+    newnode->left = newnode->right = NULL;
+    return newnode;
+}
+void insert() {
+    int data;
+    scanf("%d", &data);
+    struct node *newnode = create(data);
+    if (root == NULL) {
+        root = newnode;
+        return;
+    }
+    struct node *temp = root;
+    while (1) {
+        if (data < temp->data) {
+            if (temp->left == NULL) {
+                temp->left = newnode;
+                break;
+            }
+            temp = temp->left;
+        } else {
+            if (temp->right == NULL) {
+                temp->right = newnode;
+                break;
+            }
+            temp = temp->right;
+        }
+    }
+}
+void in_order(struct node *t) {
+    if (t) {
+        in_order(t->left);
+        printf("%d ", t->data);
+        in_order(t->right);
+    }
+}
+void pre_order(struct node *t) {
+    if (t) {
+        printf("%d ", t->data);
+        pre_order(t->left);
+        pre_order(t->right);
+    }
+}
+void post_order(struct node *t) {
+    if (t) {
+        post_order(t->left);
+        post_order(t->right);
+        printf("%d ", t->data);
+    }
+}
+void search_element() {
+    int key;
+    scanf("%d", &key);
+    struct node *temp = root;
+    while (temp) {
+        if (key == temp->data) {
+            printf("Found\n");
+            return;
+        } else if (key < temp->data)
+            temp = temp->left;
+        else
+            temp = temp->right;
+    }
+    printf("Not found\n");
 }
